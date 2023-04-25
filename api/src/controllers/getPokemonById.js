9d495614d7e060ddb513a0297f44d002d72f934a
destroctorgt <destroctorgt@gmail.com>
2023-04-25 11:39:05 -0500

    docs: se agrego comentarios al codigo

--- api/src/controllers/getPokemonById.js
+++ api/src/controllers/getPokemonById.js
@@ -1,11 +1,15 @@
 const axios = require("axios");
 
+//Esta funcion es encargada de traer un pokemon por ID desde la API o la DB y mostrarlo en pantalla.
 async function getPokemonById(req, res) {
   try {
+    //El ID viene por params //localhost:3001/pokemon/getpokemonid/:id <-
     const { id } = req.params;
 
+    //Se trae el pokemon de la API por ID.
     const { data } = await axios(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
+    //Se crea un nuevo objeto con la data recibida.
     const pokemonDetail = {
       id: data.id,
       name: data.name,
@@ -14,6 +18,7 @@ async function getPokemonById(req, res) {
       attack: data.stats[1].base_stat,
       defense: data.stats[2].base_stat,
     };
+
     res.status(200).json(pokemonDetail);
   } catch (error) {
     //Si no hubo algun otro error se retorna el error.
