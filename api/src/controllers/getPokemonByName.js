9e9f1a12c89b6a7c0486addb61674fd2d13f070e
destroctorgt <destroctorgt@gmail.com>
2023-04-27 14:38:32 -0500

    feat: se agrego la posibilidad de traer los pokemones de la db por nombre

--- api/src/controllers/getPokemonByName.js
+++ api/src/controllers/getPokemonByName.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
 
+//Requerimos al modelo Pokemon
+const { Pokemon } = require("../db");
+
 async function getPokemonByName(req, res) {
   try {
     //El name viene por query //localhost:3001/pokemon/getpokemonbyname/name?= ... <-
@@ -8,14 +11,30 @@ async function getPokemonByName(req, res) {
     //Se trae el pokemon de la API por NAME.
     const { data } = await axios(`https://pokeapi.co/api/v2/pokemon/${name}`);
 
-    //Se crea un nuevo objeto con los datos recibidos de la API.
-    const newPokemon = {
-      name: data.name,
-      image: data.sprites.other["official-artwork"]["front_default"],
-      types: pokemon.data.types,
-    };
+    //Se busca en la db el pokemon por name.
+    const pokemonDB = await Pokemon.findOne({
+      //Usamos el condicional where para que matchee con el name recibido por query.
+      where: { name: name },
 
-    res.status(200).json(newPokemon);
+      //La propieda include nos trae la info de la relacion PokemonTypes
+      include: {
+        model: Type,
+        attributes: ["name"], //usamos la propiedad attributes para que solo nos traiga el name y no el ID
+      },
+    });
+
+    console.log(pokemonDB);
+    //Se crea un nuevo array de objetos con los datos recibidos de la API y db.
+    const newPokemons = [
+      {
+        name: data.name,
+        image: data.sprites.other["official-artwork"]["front_default"],
+        types: data.types,
+      },
+      pokemonDB,
+    ];
+
+    res.status(200).json(newPokemons);
   } catch (error) {
     //Si no hubo algun otro error se retorna el error.
     res.status(404).json({ message: error });
