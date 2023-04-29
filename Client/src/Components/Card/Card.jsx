85dbf2f6e920074daea365f1d8bd3d51de1f21bd
destroctorgt <destroctorgt@gmail.com>
2023-04-29 13:17:42 -0500

    feat: se diseño la card donde se visualizara los pokemones

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import pokemonCardLogo from "../../Assets/pokemonimg4.png";
+import styles from "../Card/Card.module.css";
 
 export default function Card() {
-  return <div>Card</div>;
+  return (
+    <div className={styles.card}>
+      <img src={pokemonCardLogo} alt="pokemon card pic" />
+      <h2>Pikachu</h2>
+      <div className={styles.typeCardContainer}>
+        <div className={styles.typeCard}>
+          <h2>type</h2>
+          <p>Normal</p>
+        </div>
+      </div>
+    </div>
+  );
 }
