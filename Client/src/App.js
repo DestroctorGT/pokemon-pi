4b42585c4b1f739c4a1f4b1fe580207e151908cb
destroctorgt <destroctorgt@gmail.com>
2023-04-28 22:22:24 -0500

    feat: se agregdo la funcionalidad de cambiar de pagina a home

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import Cards from "./Components/Cards/Cards";
 
 function App() {
   return (
@@ -9,9 +10,13 @@ function App() {
       <Nav></Nav>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/home" element={<Cards />}></Route>
       </Routes>
       <footer>
-        Made with ❤️ by <a href="https://www.linkedin.com/in/gino-tapia-barrios/">Gino Tapia Barrios</a>
+        Made with ❤️ by{" "}
+        <a href="https://www.linkedin.com/in/gino-tapia-barrios/">
+          Gino Tapia Barrios
+        </a>
       </footer>
     </div>
   );
--- Client/src/Components/LandingPage/LandingPage.jsx
+++ Client/src/Components/LandingPage/LandingPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "../LandingPage/LandingPage.module.css";
 import pokemonImg from "../../Assets/pokemonimg2.png";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
     <article className={styles.landingPage}>
       <div>
         <h1>Pokemon App</h1>
-        <button>Let's begin the game</button>
+        <Link to="/home">
+          <button>Let's begin the game</button>
+        </Link>
       </div>
 
       <img
