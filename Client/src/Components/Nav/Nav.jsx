7cea49ac45d7343405d4e76efa32d57890d91f2a
destroctorgt <destroctorgt@gmail.com>
2023-04-30 10:25:40 -0500

    feat: Creado y diseñado el input para buscar pokemones

--- Client/src/Components/Nav/Nav.jsx
+++ Client/src/Components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../Assets/pokemonLogo.png";
+import pokeballInput from "../../Assets/pokeball-input.png";
 import styles from "../Nav/Nav.module.css";
 import { Link } from "react-router-dom";
 
@@ -11,6 +12,15 @@ export default function Nav() {
           <Link to="/">
             <img src={Logo} alt="pokemon logo" className={styles.logo} />
           </Link>
+        </li>
+
+        <li>
+          <div className={styles.searchBarContainer}>
+            <input
+              type="text"
+              placeholder="Search"
+              className={styles.searchBarInput}></input>
+          </div>
         </li>
       </ul>
     </nav>
