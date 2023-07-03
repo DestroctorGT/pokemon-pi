89e97cdcbd31f260c3a0b07fd2b4f99ada1aa846
Gino Tapia <destroctorgt@gmail.com>
2023-07-03 15:25:57 -0500

    feat: se creo un formulario y validaciones para iniciar sesion

--- Client/src/Components/LandingPage/LandingPage.jsx
+++ Client/src/Components/LandingPage/LandingPage.jsx
@@ -2,30 +2,108 @@ import React from "react";
 import styles from "../LandingPage/LandingPage.module.css";
 import pokemonImg from "../../Assets/pokedexLogo.webp";
 import { Link } from "react-router-dom";
+import { IconEye, IconEyeClosed } from "@tabler/icons-react";
+import { useState } from "react";
+
+//Importamos la funcion validadora.
+import validation from "./Validation";
 
 export default function LandingPage() {
+  const [iconsPassword, setIconsPassword] = useState(true);
+
+  const [iconsConfirmP, setIconsConfirmP] = useState(true);
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    error: {},
+  });
+
+  function switchIconPassword() {
+    iconsPassword ? setIconsPassword(false) : setIconsPassword(true);
+  }
+
+  function switchIconConfirmP() {
+    iconsConfirmP ? setIconsConfirmP(false) : setIconsConfirmP(true);
+  }
+
+  function handleInput(e) {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+
+    setInputs({
+      ...inputs,
+      [inputName]: inputValue,
+      error: validation(e.target),
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <article className={styles.landingPage}>
       <img src={pokemonImg} alt="landing page title pic" />
 
-      <form className={styles.registerForm}>
+      <form className={styles.registerForm} onSubmit={handleSubmit}>
         <div id={styles.inputsForm}>
           <label htmlFor="email">email</label>
-          <input type="email" id="email" placeholder="Email" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            name="email"
+            value={inputs.email}
+            onChange={handleInput}
+          />
+
+          {inputs.error.email ? (
+            <p style={{ color: "red" }}>{inputs.error.email}</p>
+          ) : null}
         </div>
 
         <div id={styles.inputsForm}>
           <label htmlFor="password">password</label>
-          <input type="password" id="password" placeholder="Password" />
+          <div>
+            <input
+              type={!iconsPassword ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+              name="password"
+              value={inputs.password}
+              onChange={handleInput}
+            />
+            <button id={styles.passwordButton} onClick={switchIconPassword}>
+              {!iconsPassword ? <IconEye /> : <IconEyeClosed />}
+            </button>
+          </div>
+
+          {inputs.error.password ? (
+            <p style={{ color: "red" }}>{inputs.error.password}</p>
+          ) : null}
         </div>
 
         <div id={styles.inputsForm}>
           <label htmlFor="confirm password">confirm password</label>
-          <input
-            type="password"
-            id="confirm password"
-            placeholder="Confirm Password"
-          />
+          <div>
+            <input
+              type={!iconsConfirmP ? "text" : "password"}
+              id="confirm password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={inputs.confirmPassword}
+              onChange={handleInput}
+            />
+            <button id={styles.passwordButton} onClick={switchIconConfirmP}>
+              {!iconsConfirmP ? <IconEye /> : <IconEyeClosed />}
+            </button>
+          </div>
+
+          {inputs.error.confirmP ? (
+            <p style={{ color: "red" }}>{inputs.error.confirmP}</p>
+          ) : null}
         </div>
 
         <div id={styles.buttonForm}>
