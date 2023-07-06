9fe70ed4619ced22b13645332ad3f45c2e24a844
Gino Tapia <destroctorgt@gmail.com>
2023-07-06 13:03:30 -0500

    feat: creado los componentes de iniciar sesion y registrarse

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,15 +5,27 @@ import LandingPage from "./Components/LandingPage/LandingPage";
 import Cards from "./Components/Cards/Cards";
 import CreatePokemon from "./Components/CreatePokemon/CreatePokemon";
 import Detail from "./Components/Detail/Detail";
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   const location = useLocation();
 
   return (
-    <div className={location.pathname === "/" ? "App" : "Home"}>
+    <div
+      className={
+        location.pathname === "/" ||
+        location.pathname === "/signIn" ||
+        location.pathname === "/signUp"
+          ? "App"
+          : "Home"
+      }
+    >
       {location.pathname === "/" ? null : <Nav></Nav>}
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/home" element={<Cards />}></Route>
         <Route path="/create" element={<CreatePokemon />}></Route>
         <Route path="/detail/:id" element={<Detail />}></Route>
